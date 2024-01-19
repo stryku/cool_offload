7b1968ef593dc46da8a287d4069def1089b55c7a
stryku <stryku2393@gmail.com>
2024-01-19 04:00:57 +0100

    Added complex work example

--- cool_offload/cool_offload.hpp
+++ cool_offload/cool_offload.hpp
@@ -24,33 +24,92 @@ class i_offloaded_work_executor {
 public:
   virtual ~i_offloaded_work_executor() = default;
 
-  // Returns number of eaten bytes
+  // Returns number of consumed bytes
   virtual std::size_t decode_and_execute(std::span<const std::byte> data) = 0;
+};
+
+class work_registry {
+public:
+  work_registry() {
+    // To create nullptr at invalid work type id.
+    m_work_type_register.resize(1);
+  }
+
+  template <typename WorkType> work_type_id_t register_work(WorkType &work) {
+    std::lock_guard lg{m_work_type_register_mutex};
+    const work_type_id_t id = m_work_type_register.size();
+    m_work_type_register.push_back(work.create_executor());
+    return id;
+  }
+
+  i_offloaded_work_executor &get_executor(work_type_id_t id) {
+    std::lock_guard lg{m_work_type_register_mutex};
+    return *m_work_type_register[id];
+  }
+
+private:
+  std::vector<std::unique_ptr<i_offloaded_work_executor>> m_work_type_register;
+  std::mutex m_work_type_register_mutex;
+};
+
+class pipe {
+public:
+  pipe() {
+    m_buffer.resize(1024);
+  }
+
+  std::span<std::byte> get_buffer(std::size_t required_space) {
+    (void)required_space;
+    return std::span(m_buffer).subspan(m_used);
+  }
+
+  void consume(std::size_t number_of_bytes) {
+    m_used += number_of_bytes;
+  }
+
+  std::span<const std::byte> get_work_buffer() {
+    return std::span(m_buffer).subspan(m_work_start, m_used - m_work_start);
+  }
+
+  void work_done(std::size_t bytes) {
+    m_work_start += bytes;
+  }
+
+private:
+  std::vector<std::byte> m_buffer;
+  std::size_t m_work_start = 0;
+  std::size_t m_used = 0;
+};
+
+struct thread_context {
+  pipe &m_pipe;
+  work_registry &m_work_type_registry;
 };
 
 template <typename Derived> class i_offloaded_work_encoder {
 public:
-  using work_type_t = std::remove_cv_t<Derived>;
-
   virtual ~i_offloaded_work_encoder() = default;
-
-  virtual std::unique_ptr<i_offloaded_work_executor> create_executor() = 0;
+  virtual std::unique_ptr<i_offloaded_work_executor>
+  create_executor() const = 0;
 
   // Returns number of used bytes in the buffer.
-  std::size_t encode(std::span<std::byte> available_space) {
+  std::size_t encode(const thread_context &context) const {
     if (m_work_type_id == 0) [[unlikely]] {
-      register_work_type();
+      m_work_type_id = context.m_work_type_registry.register_work(*this);
     }
+
+    std::span<std::byte> available_space =
+        context.m_pipe.get_buffer(required_space());
 
     assert(available_space.size() >= required_space());
 
-    std::memcpy(available_space.data(), &work_type_t::m_work_type_id,
+    std::memcpy(available_space.data(), &m_work_type_id,
                 sizeof(work_type_id_t));
 
-    available_space =
-        available_space.subspan(sizeof(work_type_t::m_work_type_id));
+    available_space = available_space.subspan(sizeof(m_work_type_id));
 
-    return static_cast<Derived &>(*this).encode_impl(available_space);
+    return sizeof(work_type_id_t) +
+           static_cast<const Derived &>(*this).encode_impl(available_space);
   }
 
   std::size_t required_space() const {
@@ -59,35 +118,66 @@ public:
   }
 
 private:
-  void register_work_type() {
-    std::lock_guard lg{detail::g_work_type_register_mutex};
-    m_work_type_id = detail::g_work_type_register.size();
-    detail::g_work_type_register.push_back(create_executor());
-  }
-
-  static inline work_type_id_t m_work_type_id = 0;
+  thread_local static inline work_type_id_t m_work_type_id = 0;
 };
 
 template <typename Derived>
 class offloaded_work : public i_offloaded_work_encoder<Derived>,
                        public i_offloaded_work_executor {};
 
+template <typename WorkType>
+void offload(const thread_context &ctx, const WorkType &work) {
+  const std::size_t consumed = work.encode(ctx);
+  ctx.m_pipe.consume(consumed);
+}
+
 class work_executor {
 public:
-  std::size_t execute_work(std::span<const std::byte> buffer) {
-    work_type_id_t id;
-    std::memcpy(&id, buffer.data(), sizeof(work_type_id_t));
-    buffer = buffer.subspan(sizeof(work_type_id_t));
+  void execute_work(const thread_context &ctx) {
 
-    i_offloaded_work_executor *work;
+    for (std::span<const std::byte> buffer = ctx.m_pipe.get_work_buffer();
+         !buffer.empty(); buffer = ctx.m_pipe.get_work_buffer()) {
 
-    {
-      std::lock_guard lg{detail::g_work_type_register_mutex};
-      work = detail::g_work_type_register[id].get();
+      work_type_id_t id;
+      std::memcpy(&id, buffer.data(), sizeof(work_type_id_t));
+      buffer = buffer.subspan(sizeof(work_type_id_t));
+
+      i_offloaded_work_executor &work =
+          ctx.m_work_type_registry.get_executor(id);
+
+      const std::size_t consumed =
+          sizeof(work_type_id_t) + work.decode_and_execute(buffer);
+      ctx.m_pipe.work_done(consumed);
     }
-
-    return sizeof(work_type_id_t) + work->decode_and_execute(buffer);
   }
+};
+
+class offload_worker {
+public:
+  thread_context create_context() {
+    m_contexts.emplace_back(std::make_unique<context>());
+    return thread_context{.m_pipe = m_contexts.back()->m_pipe,
+                          .m_work_type_registry =
+                              m_contexts.back()->m_registry};
+  }
+
+  void execute_work() {
+    work_executor executor;
+
+    for (auto &ctx : m_contexts) {
+      thread_context thread_ctx{.m_pipe = ctx->m_pipe,
+                                .m_work_type_registry = ctx->m_registry};
+      executor.execute_work(thread_ctx);
+    }
+  }
+
+private:
+  struct context {
+    work_registry m_registry;
+    pipe m_pipe;
+  };
+
+  std::vector<std::unique_ptr<context>> m_contexts;
 };
 
 } // namespace cool_offload
--- tests/cool_offload_test.cpp
+++ tests/cool_offload_test.cpp
@@ -1,6 +1,8 @@
 #include "cool_offload/cool_offload.hpp"
+#include "fmt/core.h"
 
 #include <fmt/format.h>
+#include <fmt/ranges.h>
 
 #include <gtest/gtest.h>
 #include <memory>
@@ -14,11 +16,11 @@ public:
   std::size_t decode_and_execute(std::span<const std::byte> data) final {
     unsigned answer;
     std::memcpy(&answer, data.data(), sizeof(answer));
-    fmt::print("Answer: {}\n", answer);
+    fmt::print("Test work: {}\n", answer);
     return sizeof(answer);
   }
 
-  std::unique_ptr<i_offloaded_work_executor> create_executor() final {
+  std::unique_ptr<i_offloaded_work_executor> create_executor() const final {
     return std::make_unique<test_work>();
   }
 
@@ -34,6 +36,97 @@ public:
   unsigned m_answer = 42;
 };
 
+struct complex_work_data {
+
+  std::string m_string;
+  std::vector<int> m_vector;
+
+  struct {
+    bool m_bool = true;
+    double m_double = 42.24;
+  } m_pod;
+};
+
+struct complex_work_data_decoded {
+  std::string_view m_string;
+  std::span<const int> m_vector;
+
+  struct {
+    bool m_bool = true;
+    double m_double = 42.24;
+  } m_pod;
+};
+
+class complex_work_executor final : public i_offloaded_work_executor {
+public:
+  std::size_t decode_and_execute(std::span<const std::byte> data) final {
+    complex_work_data_decoded decoded;
+
+    const std::byte *buffer = data.data();
+    std::size_t tmp;
+    std::memcpy(&tmp, buffer, sizeof(std::size_t));
+    buffer += sizeof(std::size_t);
+
+    decoded.m_string =
+        std::string_view(reinterpret_cast<const char *>(buffer), tmp);
+    buffer += tmp;
+
+    std::memcpy(&tmp, buffer, sizeof(std::size_t));
+    buffer += sizeof(std::size_t);
+    decoded.m_vector =
+        std::span<const int>(reinterpret_cast<const int *>(buffer), tmp);
+    buffer += decoded.m_vector.size_bytes();
+
+    std::memcpy(&decoded.m_pod, buffer, sizeof(decoded.m_pod));
+    buffer += sizeof(decoded.m_pod);
+
+    fmt::print("Complex work: str={}, vec={}, pod={{{}, {}}}\n",
+               decoded.m_string, decoded.m_vector, decoded.m_pod.m_bool,
+               decoded.m_pod.m_double);
+
+    return buffer - data.data();
+  }
+};
+
+class complex_work final : public i_offloaded_work_encoder<complex_work> {
+public:
+  std::unique_ptr<i_offloaded_work_executor> create_executor() const final {
+    return std::make_unique<complex_work_executor>();
+  }
+
+  constexpr std::size_t required_space_impl() const {
+    return sizeof(std::size_t) + m_data.m_string.size() + sizeof(std::size_t) +
+           m_data.m_vector.size() + sizeof(m_data.m_pod);
+  }
+
+  std::size_t encode_impl(std::span<std::byte> available_space) const {
+    std::byte *buffer = available_space.data();
+
+    std::size_t tmp = m_data.m_string.size();
+    std::memcpy(buffer, &tmp, sizeof(tmp));
+    buffer += sizeof(tmp);
+
+    std::memcpy(buffer, m_data.m_string.data(), m_data.m_string.size());
+    buffer += m_data.m_string.size();
+
+    tmp = m_data.m_vector.size();
+    std::memcpy(buffer, &tmp, sizeof(tmp));
+    buffer += sizeof(tmp);
+
+    std::memcpy(buffer, m_data.m_vector.data(),
+                m_data.m_vector.size() * sizeof(int));
+    buffer += m_data.m_vector.size() * sizeof(int);
+
+    std::memcpy(buffer, &m_data.m_pod, sizeof(m_data.m_pod));
+
+    buffer += sizeof(m_data.m_pod);
+
+    return buffer - available_space.data();
+  }
+
+  complex_work_data m_data;
+};
+
 class CoolOffloadTest : public ::testing::Test {
 public:
   void setup(std::size_t buffer_size) {
@@ -47,14 +140,24 @@ public:
   std::vector<std::byte> m_buffer;
 };
 
-TEST_F(CoolOffloadTest, EncodeDecode) {
+TEST_F(CoolOffloadTest, Basic) {
+
+  offload_worker worker;
+  thread_context ctx = worker.create_context();
 
   test_work work;
-  work.encode(m_buffer);
 
-  work_executor worker;
+  offload(ctx, work);
 
-  worker.execute_work(m_buffer);
+  complex_work complex;
+  complex.m_data.m_string = "Some cool string";
+  complex.m_data.m_vector = {1, 3, 3, 7};
+  complex.m_data.m_pod.m_bool = true;
+  complex.m_data.m_pod.m_double = 13.37;
+
+  offload(ctx, complex);
+
+  worker.execute_work();
 }
 
 } // namespace cool_offload::test
